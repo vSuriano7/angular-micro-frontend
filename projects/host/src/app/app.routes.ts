a699f7be47325c6054fd6f88e45850a19b1ff72c
suriano.vincenzo <vincenzo.suriano@dxc.com>
2024-09-02 15:33:49 +0200

    added route to thte new app

--- projects/host/src/app/app.routes.ts
+++ projects/host/src/app/app.routes.ts
@@ -6,4 +6,8 @@ export const routes: Routes = [
         path: 'my-remote-app',
         loadComponent: () => loadRemoteModule('remote', './Component').then((m) => m.AppComponent),
     },
+    {
+        path: 'my-remote-mf1',
+        loadComponent: () => loadRemoteModule('mf1', './Component').then((m) => m.AppComponent),
+    },
 ];
